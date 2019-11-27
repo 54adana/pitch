892a8cf81e7f15d33e4a4ee87e2eee9b1ea5ed5a
yvette <yvetteadan2000@gmail.com>
2019-11-27 16:03:31 +0300

    change pass_secure

--- app/models.py
+++ app/models.py
@@ -9,24 +9,28 @@ from . import login_manager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_hash = db.column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+
     
     @property
-        def password(self):
+    def password(self):
             raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
+    @password.setter
+    def password(self, password):
             self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
+    def verify_password(self,password):
             return check_password_hash(self.pass_secure,password)
     def __repr__(self):
         return f'User {self.username}'
