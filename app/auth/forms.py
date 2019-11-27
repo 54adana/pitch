8a2c1ad4104066a492dfe4e323f94e33172c3d11
yvette <yvetteadan2000@gmail.com>
2019-11-27 15:58:08 +0300

    Add password secure

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordbvField,SubmitField,ValidationError,BooleanField
+from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
     """
