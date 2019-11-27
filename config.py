0ef9b18f56834431570241c61e111ef2793d78fe
yvette <yvetteadan2000@gmail.com>
2019-11-27 16:04:46 +0300

    add uploaded photos

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ class Config:
 
     SECRET_KEY = "try harder" #os.environ.get("SECRET_KEY")
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 
