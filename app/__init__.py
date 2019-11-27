3491338fba73e26b2789de4652def7dfc8d98279
yvette <yvetteadan2000@gmail.com>
2019-11-27 15:53:39 +0300

    Add profile photo

--- app/__init__.py
+++ app/__init__.py
@@ -5,33 +5,43 @@ from flask_sqlalchemy import SQLAlchemy
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+mail = Mail()
 
-def create_app(config_name):
-    #....
-    #Initializing Flask Extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
 
+# def create_app(config_name):
+#     #....
+#     #Initializing Flask Extensions
+#     bootstrap.init_app(app)
+#     db.init_app(app)
+#     login_manager.init_app(app)
+
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
 
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    mail.init_app(app)
+    
+     # configure UploadSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-     login_manager.init_app(app)
+    login_manager.init_app(app)
     from . main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .auth import as auth_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     
