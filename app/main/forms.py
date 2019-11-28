f5b42bef04106d72d8ac29d986fa5617f93862e0
yvette <yvetteadan2000@gmail.com>
2019-11-28 15:51:09 +0300

    add classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,14 +2,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
+from wtforms.validators import DataRequired
+
+
+# class PitchForm(FlaskForm):
+#     '''
+#     Class to create a wtf form for creating a pitch
+#     '''
+#     content = TextAreaField('YOUR PITCH')
+#     submit = SubmitField('SUBMIT')
 
 
 class PitchForm(FlaskForm):
-    '''
-    Class to create a wtf form for creating a pitch
-    '''
-    content = TextAreaField('YOUR PITCH')
-    submit = SubmitField('SUBMIT')
+     title = StringField('Title', validators=[DataRequired()])
+     content = TextAreaField('Content',validators=[DataRequired()])
+     submit = SubmitField('Pitch')
 
 class CommentForm(FlaskForm):
     '''
